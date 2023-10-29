34adf9186f9cdbdfdff4863e9c2f1a04dfdaf7b4
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-29 12:12:45 -0700

    basic

---
+++ basic.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+    return "Hello world"
+
+@app.route("/print")
+def print(name):
+    return f"Hello {name}"
+if __name__ == "__main__":
+    app.run()
