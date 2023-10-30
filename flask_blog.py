9f1c2181a3ceb7302bc29438de460d3b88b0a9f9
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-30 11:56:23 -0700

    basic

--- flask_blog.py
+++ flask_blog.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return "Hello"
+    return "<h1>Hello word!</h1>"
+
+@app.route("/about")
+def about():
+    return "<h2>This is about page</h2>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
