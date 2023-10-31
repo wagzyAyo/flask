9a0c96fbc0a063663af8081689f732cf46a70e09
wagzyAyo <talktojmcvibes@gmail.com>
2023-10-31 02:49:00 -0700

    template

--- flask_blog.py
+++ flask_blog.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+data = [
+    {
+        "author": "James  Bond",
+        "title": "Blog Post1",
+        "content": "New blog post on 999 street 2022",
+        "date": "20/8/2020"
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post1",
+        "content": "New blog post on 999 street 2022",
+        "date": "20/8/2020"
+    }
+]
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello word!</h1>"
+    return render_template("home.html", posts=data)
 
 @app.route("/about")
 def about():
-    return "<h2>This is about page</h2>"
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
